eec9828d71316cbca899fa6c879e3a25459af03c
Yuri Buenos Aires Santana <yuri.santana@discente.ufg.br>
2024-05-04 14:22:53 -0300

    comecando av1

--- mlops/app.py
+++ mlops/app.py
@@ -1,66 +1,16 @@
 import os
 from flask import Flask, request, jsonify
-from joblib import load
+# Importar Biblioteca da Hugging Face
+from transformers import pipeline
 
 app = Flask(__name__)
 
-# Obter o caminho completo para o arquivo modelo.pkl na pasta models
-modelo_path = os.path.join('models', 'modelo.pkl')
+# Carregar o modelo de classificação de sentimentos
+sentiment_model = pipeline("sentiment-analysis")
 
-# Carregar o modelo
-modelo = load(modelo_path)
 
-# Dados fictícios de exemplo
-exemplo = {
-    "fixed acidity": 7.0,
-    "volatile acidity": 0.27,
-    "citric acid": 0.36,
-    "residual sugar": 20.7,
-    "chlorides": 0.045,
-    "free sulfur dioxide": 45.0,
-    "total sulfur dioxide": 170.0,
-    "density": 1.0010,
-    "pH": 3.00,
-    "sulphates": 0.45,
-    "alcohol": 8.8,
-    "color": 1
-}
-
-# Teste no SO a partir de um "arquivo.json". 
-# curl -X POST -H "Content-Type: application/json" -d @arquivo.json http://localhost:5000/predict
-
-# Teste no SO com JSON inline. 
-# curl -X POST -H "Content-Type: application/json" -d '{"fixed acidity":6.6,"volatile acidity":0.16,"citric acid":0.4,"residual sugar":1.5,"chlorides":0.044,"free sulfur dioxide":48.0,"total sulfur dioxide":143.0,"density":0.9912,"pH":3.54,"sulphates":0.52,"alcohol":12.4,"color":1}' http://localhost:8500/predict
-
-""" Teste no SO com JSON formatado. 
-# curl -X POST -H "Content-Type: application/json" \
--d '{
-    "fixed acidity": 6.6,
-    "volatile acidity": 0.16,
-    "citric acid": 0.4,
-    "residual sugar": 1.5,
-    "chlorides": 0.044,
-    "free sulfur dioxide": 48.0,
-    "total sulfur dioxide": 143.0,
-    "density": 0.9912,
-    "pH": 3.54,
-    "sulphates": 0.52,
-    "alcohol": 12.4,
-    "color": 1
-}' \
-http://localhost:8500/predict
-
-"""
-
-@app.route('/test', methods=['GET'])
-def test():
-    # Fazer uma previsão de exemplo
-    previsao = modelo.predict([list(exemplo.values())])
-    return jsonify(previsao.tolist())
-
-# Definir rota para receber requisições POST
-@app.route('/predict', methods=['POST'])
-def predict():
+@app.route('/analyze_sentiment',methods=['POST'])
+def analyze_sentiment():
     # Receber dados JSON da requisição
     data = request.get_json()
 
@@ -87,6 +37,6 @@ def predict():
     # Retornar o resultado como JSON
     return jsonify(resultado)
 
-# Executar o aplicativo Flask
+    # Executar o aplicativo Flask
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
